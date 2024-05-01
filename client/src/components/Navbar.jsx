ce59c2d913f91a91cee770572fec0d0c0ece1228
kumar-abhi001 <kumar.abhi.ok@gmail.com>
2024-05-02 00:37:46 +0530

    Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,71 @@
 const Navbar = () => {
     return (
-        <p>This is navbar</p>
-    )
+      <div className="container-fluid flex flex-row border-2 p-2">
+        <button
+          type="button"
+          id="navbarToggler"
+          className="d-lg-none ps-0 btn btn-link"
+        >
+          <span className="navbar-toggler-icon">Icon</span>
+        </button>
+        <a className="me-auto" id="headerLogo">
+          Logo
+        </a>
+        <div className="d-flex align-items-center flex justify-between mr-11">
+          <div className="relative">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg
+                class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
+            </div>
+            <input
+              type="search"
+              id="default-search"
+              class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Search"
+              required
+            />
+            <button
+              type="submit"
+              class="text-white absolute end-1 bottom-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Search
+            </button>
+          </div>
+          <div className="d-none d-md-block">
+            <button
+              type="button"
+              id="signup"
+                className="bg-blue-700 hover:bg-blue-800
+               text-white font-bold py-3 px-4 rounded-3xl ml-3 mr-3"
+            >
+              Signup
+            </button>
+                    
+            <button
+              type="button"
+              id="login"
+                className="bg-blue-700 hover:bg-blue-800
+               text-white font-bold py-3 px-4 rounded-3xl"
+            >
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    );
 }
 
 export default Navbar;
